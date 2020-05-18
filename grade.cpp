99c8bbe74a03963dcad56bba0a35de2778423a14
Tia Benson <51142215+tiabenson@users.noreply.github.com>
2020-05-17 21:30:13 -0500

    Added GPA functionality, haven't tested

--- grade.cpp
+++ grade.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <string>
 
 void calculate()
 {
@@ -41,7 +42,7 @@ void calculate()
         p += percent[i];
     }
     
-    std::cout<<"\nYour average grade is "<<std::fixed<<std::setprecision(2)<<(total/p)<<std::endl;
+    std::cout<<"\nYour average grade is "<<(total/p)<<std::endl;
     
     if((total/p) >= 90 && (total/p) <= 100) //determine letter grade
     {
@@ -64,31 +65,150 @@ void calculate()
     }
 }
 
-int main(int argc, char ** argv)
+void GPA()
 {
-    std::cout<<"\n** Weighted Grade Calculator **\n"<<std::endl;
-    bool check = true;
-    std::string ans;
+    int num = 1;
+    std::string grade;
+    int credit;
+    int total = 0, total_credits = 0;
+    std::vector<std::string> letters;
+    std::vector<int> credits;
     
-    while (check)
+    while(true)
     {
-        calculate();
+        std::cout<<"Enter grade letter of class #"<<num<<": ";
+        std::cin>>grade;
         
-        std::cout<<"\n\nWould you like to calculate another average? (Y/N): ";
-        std::cin>>ans;
-        
-        if(ans == "Y" || ans == "y" || ans == "Yes" || ans == "yes") //yes
+        if(grade == (std::to_string(00))) //stop taking grades if 00 is typed
         {
-            std::cout<<"\n";
+            break;
         }
         
-        else //no
+        else
         {
-            std::cout<<"\n\n***Thank you for using the grade calculator!***\n\n";
-            break;
+            letters.push_back(grade);
+        }
+        
+        std::cout<<"Enter number of credits of class #"<<num<<": ";
+        std::cin>>credit;
+        
+        credits.push_back(credit);
+        num++;
+        std::cout<<"\n";
+    }
+    
+    for(int i = 0; i < credits.size(); i++)
+    {
+        if(letters[i] == "A" || letters[i] == "a")
+        {
+            total += 4 * credits[i];
+            total_credits += credits[i];
+        }
+        
+        else if(letters[i] == "B" || letters[i] == "b")
+        {
+            total += 3 * credits[i];
+            total_credits += credits[i];
+        }
+        
+        else if(letters[i] == "C" || letters[i] == "c")
+        {
+            total += 2 * credits[i];
+            total_credits += credits[i];
+        }
+        
+        else if(letters[i] == "D" || letters[i] == "d")
+        {
+            total += 1 * credits[i];
+            total_credits += credits[i];
+        }
+        
+        else    //assume the user typed a failing grade letter
+        {
+            total += 0 * credits[i];
+            total_credits += credits[i];
         }
     }
     
+    float gpa = total/total_credits;
+    
+    std::cout<<"Your GPA is "<<gpa<<std::endl;
+ 
+}
+
+int main(int argc, char ** argv)
+{
+    bool check = true;
+    
+    while(check)
+    {
+        int option = 0;
+        
+        std::cout<<"\n** Weighted Grade Calculator **\n"<<std::endl;
+        std::cout<<"Please type 1, 2, or 3 to select what you want to calculate:"<<std::endl;
+        std::cout<<"1: Weighted Grade"<<std::endl;
+        std::cout<<"2: GPA"<<std::endl;
+        std::cout<<"3: Quit"<<std::endl;
+        std::cin>>option;
+        std::string ans;
+        
+        if(option == 1)
+        {
+            while(1)
+            {
+                calculate();
+                
+                std::cout<<"\n\nWould you like to calculate another average? (Y/N): ";
+                std::cin>>ans;
+                
+                if(ans == "Y" || ans == "y" || ans == "Yes" || ans == "yes") //yes
+                {
+                    std::cout<<"\n";
+                }
+                
+                else //if no is typed
+                {
+                    std::cout<<"\n***Thank you for using the weighted grade calculator!***\n\n";
+                    break;
+                }
+            }
+        }
+        
+        else if(option == 2)
+        {
+            while(1)
+            {
+                GPA();
+                
+                std::cout<<"\n\nWould you like to calculate another GPA? (Y/N): ";
+                std::cin>>ans;
+                
+                if(ans == "Y" || ans == "y" || ans == "Yes" || ans == "yes") //yes
+                {
+                    std::cout<<"\n";
+                }
+                
+                else //if no is typed
+                {
+                    std::cout<<"\n***Thank you for using the GPA calculator!***\n\n";
+                    break;
+                }
+            }
+        }
+        
+        else if(option == 3)
+        {
+            std::cout<<"\n***Thank you for using the grade calculator!***\n\n";
+            exit(0);
+        }
+        
+        else
+        {
+            std::cout<<"Option not recognized. Please try again."<<std::endl;
+            continue;
+        }
+    }
+ 
     return 0;
 }
 
